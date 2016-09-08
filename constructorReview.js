fc947167cee310456975f9bede4a5706c6875acb
User <User@CQ.local>
2016-09-08 14:25:02 -0600

    added text

--- constructorReview.js
+++ constructorReview.js
@@ -1,34 +1,58 @@
 //Create a Animal Constructor that has the following parameters. species, name, legs, color, food (which is an array of foods they can eat).
 
-  //code here
+var Animal = function(species, name, legs, color, food){
+  this.species = species;
+  this.name = name;
+  this.legs = legs;
+  this.color = color;
+  this.food = this.food;
+  this.eat = function(){
+    console.log(this.name + ' ate ' + food);
+  }
+}
+Animal.prototype.food = ['Meat', 'pig', 'all of the food'];
+
 
 
 //Now create a person function that creates an object and returns it (not in constructor form) that has the following parameters. name, age, height, gender
 
-  //code here
+  var person = function(name, age, height, gender){
+    return{
+      name: name,
+      age: age,
+      height: height,
+      gender: gender
+    }
+  }
 
 
 //Create a animal array and a person array.
 
-  //code here
+  var animal = []
+  var person = []
 
 
 //Create two instances of Animal and push those into your animal array
 
-  //code here
+  var tiger = new Animal('cat', 'Tiger', 4, 'Orange Black', 'Meat')
+  var wolf = new Animal('dog', 'Wolf', 4, 'gray', 'pig')
+  animal.push(tiger, wolf)
 
 
 //Create two instances of person and push those into your person array.
 
-  //code here
+  var user1 = new person('Noah', 20, '5,9', 'Male')
+  var user2 = new person('kelsie', 21, '5,5', 'Female')
+  person.push(user1, user2)
 
 
 //Now we want every instance of Animal to have a eat method. This method will choose a random item in that instances food array, then alert "(name) ' ate ' (whichever food was chosen)".
 
-  //code here
+  tiger.eat();
+  wolf.eat();
 
 
-//At this point, if we wanted to add something to every istance of person could we? 
+//At this point, if we wanted to add something to every istance of person could we?
 
   //Yes or no? and why or why not?
 
